db98974670f268ec1c35caca910dd081ff791bfd
xubo_dev <810097239@qq.com>
2020-07-01 10:47:56 +0800

    1.用户权限数据表结构

---
+++ src/main/resources/springboot_study.sql
@@ -0,0 +1,76 @@
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+                      `id` int(11) NOT NULL AUTO_INCREMENT,
+                      `user_id` varchar(20) NOT NULL COMMENT '用户id',
+                      `username` varchar(50) NOT NULL COMMENT '用户名',
+                      `password` varchar(50) NOT NULL,
+                      `salt` varchar(128) DEFAULT NULL COMMENT '加密盐值',
+                      `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+                      `phone` varchar(50) DEFAULT NULL COMMENT '联系方式',
+                      `sex` int(255) DEFAULT NULL COMMENT '年龄：1男2女',
+                      `age` int(3) DEFAULT NULL COMMENT '年龄',
+                      `status` int(1) NOT NULL COMMENT '用户状态：1有效; 2删除',
+                      `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                      `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+                      `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
+                      PRIMARY KEY (`id`,`user_id`),
+                      UNIQUE KEY `a` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+                      `id` int(11) NOT NULL AUTO_INCREMENT,
+                      `role_id` varchar(20) NOT NULL COMMENT '角色id',
+                      `name` varchar(50) NOT NULL COMMENT '角色名称',
+                      `description` varchar(255) DEFAULT NULL COMMENT '角色描述',
+                      `status` int(1) NOT NULL COMMENT '状态：1有效；2删除',
+                      `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                      `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+                      PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+                            `id` int(11) NOT NULL AUTO_INCREMENT,
+                            `permission_id` varchar(20) NOT NULL COMMENT '权限id',
+                            `name` varchar(100) NOT NULL COMMENT '权限名称',
+                            `description` varchar(255) DEFAULT NULL COMMENT '权限描述',
+                            `url` varchar(255) DEFAULT NULL COMMENT '权限访问路径',
+                            `perms` varchar(255) DEFAULT NULL COMMENT '权限标识',
+                            `parent_id` int(11) DEFAULT NULL COMMENT '父级权限id',
+                            `type` int(1) DEFAULT NULL COMMENT '类型   0：目录   1：菜单   2：按钮',
+                            `order_num` int(3) DEFAULT '0' COMMENT '排序',
+                            `icon` varchar(50) DEFAULT NULL COMMENT '图标',
+                            `status` int(1) NOT NULL COMMENT '状态：1有效；2删除',
+                            `create_time` datetime DEFAULT NULL,
+                            `update_time` datetime DEFAULT NULL,
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission` (
+                                 `id` int(11) NOT NULL AUTO_INCREMENT,
+                                 `role_id` varchar(20) NOT NULL COMMENT '角色id',
+                                 `permission_id` varchar(20) NOT NULL COMMENT '权限id',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=869 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `user_id` varchar(20) NOT NULL COMMENT '用户id',
+                           `role_id` varchar(20) NOT NULL COMMENT '角色id',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
